47bca3e38aac9ff1947834b7ae8f9acdd266ba0b
codedilla <nick@qodbrains.com>
2022-03-06 01:23:53 +0200

    Navigation Style

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,32 @@
-/*
-Please rename this file as you see fit. GitHub doesn't allow the creation, or pushing of
-    empty folders, so I had to place something.
+// MOBILE/ HAMBURGER NAVIGATION SCRIPT
 
-    - qod-dante
-*/
+const hamburgerButton = document.querySelector('.hamburger-container');
+const mobileNavigation = document.querySelector('.mobile-navigation');
+const backDrop = document.querySelector('.back-drop');
+
+let navigationOpen = false;
+
+hamburgerButton.addEventListener('click', function () {
+    if (!navigationOpen) {
+        mobileNavigation.style.display = 'block';
+        backDrop.style.display = 'block';
+        navigationOpen = true;
+        
+    } else {
+        mobileNavigation.style.display = 'none';
+        backDrop.style.display = 'none';
+        navigationOpen = false;
+        
+    }
+});
+
+backDrop.addEventListener('click', function() {
+    if(!navigationOpen) {
+    navigationOpen = true;
+    } else {
+        mobileNavigation.style.display = 'none';
+        backDrop.style.display = 'none';
+        navigationOpen = false;
+    }
+})
+// MOBILE/ HAMBURGER NAVIGATION SCRIPT ENDS!!
