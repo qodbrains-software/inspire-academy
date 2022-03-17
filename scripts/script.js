b2fb388683e08679e175224ca3c1b1b89b2b51bb
sonwabi <sona@qodbrains.com>
2022-03-17 16:09:33 +0200

    adding carousel

--- scripts/script.js
+++ scripts/script.js
@@ -35,84 +35,69 @@ backDrop.addEventListener('click', function() {
 
 const track = document.querySelector(".main-container");
 const slides = Array.from(track.children);
-const nextbtn = document.querySelector(".next");
-const prevbtn = document.querySelector(".prev");
 const dotsContainer = document.querySelector(".dotsContainer");
 const dots = Array.from(dotsContainer.children);
 
-
-const slideWidth = slides[0].getBoundingClientRect().width;
-// slides[0].style.left = slideWidth * 0 + "px";
-// slides[1].style.left = slideWidth * 1 + "px";
-// slides[2].style.left = slideWidth * 2 + "px";
-
-const setslides = (slide, index) => {
-    slide.style.left = slideWidth * index + "px";
-};
-
-slides.forEach(setslides);
-
-const moveToSlide = (track, firstslide, targetSlide) => {
-    track.style.transform = "translatex(-" + targetSlide.style.left + ")"
-    firstslide.classList.remove("first");
-    targetSlide.classList.add("first");
-}
+let pause = false;
 
 // manual carousel
-nextbtn.addEventListener("click", e => {
-    const firstslide = track.querySelector(".first");
-    const nextslide = firstslide.nextElementSibling;
-
-    moveToSlide(track, firstslide, nextslide);
-})
-
-prevbtn.addEventListener("click", e => {
-    const firstslide = track.querySelector(".first");
-    const prevslide = firstslide.previousElementSibling;
-
-    moveToSlide(track, firstslide, prevslide);
-})
-
 
 dotsContainer.addEventListener("click", e => {
-    const targetDot = e.target.closest("i");
 
-    if (!targetDot) return;
+    const dots = document.querySelectorAll(".dots");
+    const targetClass = e.target.classList[1];
 
-    const firstslide = track.querySelector(".first");
-    const currentDot = dotsContainer.querySelector("first");
-    const targetIndex = dots.findIndex(dots => dots === targetDot);
-    const targetSlide = slides[targetIndex];
-
-    moveToSlide(track, firstslide, targetSlide);
+    switch (targetClass) {
+        case "dots1":
+            pause = true;
+            slides[0].style.display = "flex";
+            slides[1].style.display = "none";
+            slides[2].style.display = "none";
+            break;
+        case "dots2":
+            pause = true;
+            slides[1].style.display = "flex";
+            slides[0].style.display = "none";
+            slides[2].style.display = "none";
+            break;
+        case "dots3":
+            pause = true;
+            slides[2].style.display = "flex";
+            slides[1].style.display = "none";
+            slides[0].style.display = "none";
+            break;
+    }
 })
-
-
-// const slider = () => {
-//     let i = 0;
-//     setInterval(() => {
-//         moveToSlide(track, slides[i], slides[i + 1])
-//         i++;
-//         if (i == 2) {
-//             i = 0;
-//         }
-//     }, 6000)
-// }
-
-// slider();
 
 let slider = () => {
     let index = 0;
 
-    setInterval(() => {
-        slides[index].style.display = "none";
-        index++;
 
+    setInterval(() => {
+        if (!pause) {
+            switch (index) {
+                case 0:
+                    slides[0].style.display = "flex";
+                    slides[1].style.display = "none";
+                    slides[2].style.display = "none";
+                    break;
+                case 1:
+                    slides[1].style.display = "flex";
+                    slides[0].style.display = "none";
+                    slides[2].style.display = "none";
+                    break;
+                case 2:
+                    slides[2].style.display = "flex";
+                    slides[1].style.display = "none";
+                    slides[0].style.display = "none";
+                    break;
+
+            }
+        }
+
+        index++;
         if (index === 3) {
             index = 0;
-            slides[0].style.display = "flex";
-            slides[1].style.display = "flex";
-            slides[2].style.display = "flex";
         }
 
     }, 4000)
