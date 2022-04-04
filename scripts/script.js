f4714f6ee20e53e6c06d387828d57738decb71e5
sonwabi <sona@qodbrains.com>
2022-04-04 11:42:28 +0200

    console errors fixed

--- scripts/script.js
+++ scripts/script.js
@@ -77,23 +77,23 @@ backDrop.addEventListener('click', function() {
 // Payment modal
 
 
-const hide = document.querySelector(".main-method");
-const btn_payment = document.querySelector(".buybook");
-const btn_2 = document.querySelector(".buy2");
-const btn_3 = document.querySelector(".buy3");
-const close_btn = document.querySelector(".icon");
+// const hide = document.querySelector(".main-method");
+// const btn_payment = document.querySelector(".buybook");
+// const btn_2 = document.querySelector(".buy2");
+// const btn_3 = document.querySelector(".buy3");
+// const close_btn = document.querySelector(".icon");
 
-function payment() {
-    hide.style.display = "flex";
-    modalDrop.style.display = 'block';
-}
+// function payment() {
+//     hide.style.display = "flex";
+//     modalDrop.style.display = 'block';
+// }
 
-function close() {
-    hide.style.display = "none";
-    modalDrop.style.display = 'none';
-}
+// function close() {
+//     hide.style.display = "none";
+//     modalDrop.style.display = 'none';
+// }
 
-close_btn.addEventListener("click", close);
-btn_payment.addEventListener("click", payment);
-btn_2.addEventListener("click", payment);
-btn_3.addEventListener("click", payment);
+// close_btn.addEventListener("click", close);
+// btn_payment.addEventListener("click", payment);
+// // btn_2.addEventListener("click", payment);
+// // btn_3.addEventListener("click", payment);
